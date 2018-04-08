107770ecdcb820d156830595984363874933ad19
jmoe <james.moessis1@gmail.com>
2018-04-08 21:51:56 +1000

    test webhook

--- test_run.c
+++ test_run.c
@@ -1,1 +1,7 @@
+#include <stdio.h>
 
+int main(void){
+    printf("This code is built when dust_sensor repo gets committed to master.\n"
+            "GitHub POSTs to Jenkins server, which triggers a build!");
+}
+    return 0;
