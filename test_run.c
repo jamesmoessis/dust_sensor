57a286dc0d5b79d66a9c1319c0c5734ca0324b58
James Moessis <James.G.Moessis@student.uts.edu.au>
2018-04-08 22:07:15 +1000

    newlines for readability

--- test_run.c
+++ test_run.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void){
-    printf("This code is built when dust_sensor repo gets committed to master.\n"
-            "GitHub POSTs to Jenkins server, which triggers a build!");
+    printf("\nThis code is built when dust_sensor repo gets committed to master.\n"
+            "GitHub POSTs to Jenkins server, which triggers a build!\n");
     return 0;
 }
     
