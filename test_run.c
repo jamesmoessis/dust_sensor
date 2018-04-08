6722d69b308dcd85a520aef28f628acc924f2ed1
James Moessis <James.G.Moessis@student.uts.edu.au>
2018-04-08 22:05:24 +1000

    forgot to return main :O

--- test_run.c
+++ test_run.c
@@ -3,5 +3,6 @@
 int main(void){
     printf("This code is built when dust_sensor repo gets committed to master.\n"
             "GitHub POSTs to Jenkins server, which triggers a build!");
-}
     return 0;
+}
+    
