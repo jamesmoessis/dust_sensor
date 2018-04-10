6b3ea36286afaa97e928dc161c146593ee96b73a
jmoe <james.moessis1@gmail.com>
2018-04-10 22:57:40 +1000

    fix annoying arduino magic

--- dust_sensor.ino
+++ dust_sensor.ino
@@ -1,54 +1,62 @@
-int readPot(void);
+/*
+  Analog input, analog output, serial output
 
-// These are used to give names to the pins:
+  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
+  the result to set the pulse width modulation (PWM) of an output pin.
+  Also prints the results to the Serial Monitor.
+
+  The circuit:
+  - potentiometer connected to analog pin 0.
+    Center pin of the potentiometer goes to the analog pin.
+    side pins of the potentiometer go to +5V and ground
+  - LED connected from digital pin 9 to ground
+
+  created 29 Dec. 2008
+  modified 9 Apr 2012
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
+*/
+
+void read_adc();
+
+// These constants won't change. They're used to give names to the pins used:
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
 const int analogOutPin = 9; // Analog output pin that the LED is attached to
 
-int potValue = 0;        // value read from the pot
-int outputPot = 0;       // value output to the PWM (analog out)
-
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
-
+  Serial.println("Started!");
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  readPot();
+  read_adc();
 }
 
 
 
-
-
-
-
-
-
-
-
-int readPot(void){
-  // This function is from Arduino Library
-  // http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
-  
+void read_adc(){
   // read the analog in value:
-  potValue = analogRead(analogInPin);
+  sensorValue = analogRead(analogInPin);
   // map it to the range of the analog out:
-  outputPot = map(outputPot, 0, 1023, 0, 255);
+  outputValue = map(sensorValue, 0, 1023, 0, 255);
   // change the analog out value:
-  analogWrite(analogOutPin, outputPot);
+  analogWrite(analogOutPin, outputValue);
 
   // print the results to the Serial Monitor:
   Serial.print("sensor = ");
-  Serial.print(potValue);
+  Serial.print(sensorValue);
   Serial.print("\t output = ");
-  Serial.println(outputPot);
+  Serial.println(outputValue);
 
   // wait 2 milliseconds before the next loop for the analog-to-digital
   // converter to settle after the last reading:
   delay(500);
-  return 0;
 }
-
