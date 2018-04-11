577ab9553e0d1b1b86fb11cf1be10f2e3c577096
jmoe <james.moessis1@gmail.com>
2018-04-11 20:27:28 +1000

    add library

---
+++ HPMA115S0/README.md
@@ -0,0 +1,4 @@
+# HPMA115S0
+Library for Honeywell's Particle Sensor (HPMA115S0-XXX)
+
+Credit for this folder of code goes to https://github.com/felixgalindo/HPMA115S0
--- dust_sensor.ino
+++ dust_sensor.ino
@@ -1,26 +1,4 @@
-/*
-  Analog input, analog output, serial output
-
-  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
-  the result to set the pulse width modulation (PWM) of an output pin.
-  Also prints the results to the Serial Monitor.
-
-  The circuit:
-  - potentiometer connected to analog pin 0.
-    Center pin of the potentiometer goes to the analog pin.
-    side pins of the potentiometer go to +5V and ground
-  - LED connected from digital pin 9 to ground
-
-  created 29 Dec. 2008
-  modified 9 Apr 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
-*/
-
-void read_adc();
+int read_adc();
 
 // These constants won't change. They're used to give names to the pins used:
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
@@ -29,15 +7,21 @@ const int analogOutPin = 9; // Analog output pin that the LED is attached to
 int sensorValue = 0;        // value read from the pot
 int outputValue = 0;        // value output to the PWM (analog out)
 
+
+
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
-  Serial.println("Started!");
-  
+  Serial.println("Setup complete!");
 }
 
+
 void loop() {
-  read_adc();
+    // threshold dust level out of 100
+    float threshold;
+    threshold = read_adc() * 100.0f/255.0f;
+
+
 }
 
 
@@ -58,5 +42,6 @@ void read_adc(){
 
   // wait 2 milliseconds before the next loop for the analog-to-digital
   // converter to settle after the last reading:
-  delay(500);
+  delay(2);
+  return outputValue;
 }
