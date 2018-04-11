e072f5a6be70e40e5d18bc4216bb14145e852d61
jmoe <james.moessis1@gmail.com>
2018-04-11 22:45:22 +1000

    time for bed. Initialization of Serials attempted. Not currently working.

--- HPMA115S0/README.md
+++ HPMA115S0/README.md
@@ -1,4 +1,2 @@
 # HPMA115S0
 Library for Honeywell's Particle Sensor (HPMA115S0-XXX)
-
-Credit for this folder of code goes to https://github.com/felixgalindo/HPMA115S0
--- dust_sensor.ino
+++ dust_sensor.ino
@@ -1,7 +1,8 @@
 #include "Arduino.h"
-#include "./HPMA115S0/src/HPMA115S0.h"
+#include "HPMA115S0/src/hpma115S0.h"
+#include "HPMA115S0/src/hpma115S0.cpp"
 
-#define BAUDRATE 9600;
+#define BAUDRATE 9600
 // These are used to give names to the pins used:
 const int analogInPin = A0;  // Potentiometer Analog ping
 const int analogOutPin = 9; 
@@ -16,11 +17,13 @@ HPMA115S0 honeywell(Serial1);
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(BAUDRATE); //begin adc comms
-  Serial1.begin(BAUDRATE);//begin honeywell comms
+  Serial.println("Hello Serial");
+  Serial1.begin(BAUDRATE); //begin honeywell comms
   while (!Serial1) {
-    ; // wait for honeywell to connect
+    delay(500); // wait for honeywell to connect
+    Serial.println("Starting Serial1...");
   }
-  Serial.println("Starting...");
+  Serial1.println("Hello Serial 1, you old honeywell\n");
   honeywell.Init();
   honeywell.StartParticleMeasurement();
   Serial.println("Setup func complete!");
@@ -31,13 +34,19 @@ void loop() {
     // threshold dust level out of 100
     float threshold;
     threshold = read_adc() * 100.0f/255.0f;
+    
+    //honeywell read
+    unsigned int pm2_5, pm10;
+    if (honeywell.ReadParticleMeasurement(&pm2_5, &pm10)) {
+    Serial1.println("PM 2.5: " + String(pm2_5) + " ug/m3" );
+    Serial1.println("PM 10: " + String(pm10) + " ug/m3" );
+  }
 
 
 }
 
 
-
-void read_adc(){
+int read_adc(){
   // read the analog in value:
   sensorValue = analogRead(analogInPin);
   // map it to the range of the analog out:
