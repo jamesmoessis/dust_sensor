b9443dbf151f8d2e4d6fc674459ebcba64fc7224
unknown <james.moessis1@gmail.com>
2018-04-15 21:36:31 +1000

    dust measurement working.

--- dust_sensor.ino
+++ dust_sensor.ino
@@ -1,29 +1,35 @@
+/*
+ * @file dust_sensor.ino
+ * @author James Moessis
+ * @date April 2018
+ */
+
 #include "Arduino.h"
-#include "HPMA115S0/src/hpma115S0.h"
-#include "HPMA115S0/src/hpma115S0.cpp"
+#include <hpma115S0.h>
 
 #define BAUDRATE 9600
+
 // These are used to give names to the pins used:
 const int analogInPin = A0;  // Potentiometer Analog ping
 const int analogOutPin = 9; 
 int sensorValue = 0;        // value read from the pot
 int outputValue = 0;        // value output to the PWM
+float threshold;
 
-int read_adc(); //definition
+//int read_adc(); //definition
 
 //Create an instance of the HPMA115S0 library
 HPMA115S0 honeywell(Serial1);
 
 void setup() {
   // initialize serial communications at 9600 bps:
-  Serial.begin(BAUDRATE); //begin adc comms
-  Serial.println("Hello Serial");
+  Serial.begin(BAUDRATE); // begin comms with USB (to Arduino Serial Monitor)
+  Serial.println("Hello Computer.");
   Serial1.begin(BAUDRATE); //begin honeywell comms
-  while (!Serial1) {
-    delay(500); // wait for honeywell to connect
+  do {
+    delay(5000); // wait for honeywell to connect
     Serial.println("Starting Serial1...");
-  }
-  Serial1.println("Hello Serial 1, you old honeywell\n");
+  } while (!Serial1);
   honeywell.Init();
   honeywell.StartParticleMeasurement();
   Serial.println("Setup func complete!");
@@ -32,20 +38,18 @@ void setup() {
 
 void loop() {
     // threshold dust level out of 100
-    float threshold;
     threshold = read_adc() * 100.0f/255.0f;
     
     //honeywell read
     unsigned int pm2_5, pm10;
     if (honeywell.ReadParticleMeasurement(&pm2_5, &pm10)) {
-    Serial1.println("PM 2.5: " + String(pm2_5) + " ug/m3" );
-    Serial1.println("PM 10: " + String(pm10) + " ug/m3" );
+    Serial.println("PM 2.5: " + String(pm2_5) + " ug/m3" );
+    Serial.println("PM 10: " + String(pm10) + " ug/m3" );
   }
-
-
+  
 }
 
-
+//adapted from example Arduino sketch
 int read_adc(){
   // read the analog in value:
   sensorValue = analogRead(analogInPin);
@@ -65,3 +69,4 @@ int read_adc(){
   delay(2);
   return outputValue;
 }
+
