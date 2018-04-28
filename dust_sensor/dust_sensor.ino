0f04fa48753fffd2e0306c774e98ef2ac76adc27
James Moessis <james.moessis1@gmail.com>
2018-04-28 15:25:30 +1000

    monitoring working. timeplot and logging to MegunoLink

--- dust_sensor/dust_sensor.ino
+++ dust_sensor/dust_sensor.ino
@@ -8,25 +8,27 @@
  */
 
 #include "Arduino.h"
-#include <hpma115S0.h> // https://github.com/GoldenMoe/HPMA115S0
-#include <TimeLib.h>   // https://github.com/PaulStoffregen/Time
+#include <hpma115S0.h>   // https://github.com/GoldenMoe/HPMA115S0
+#include <TimeLib.h>     // https://github.com/PaulStoffregen/Time
+#include <MegunoLink.h>  // https://github.com/Megunolink/MLP
 
 #define BAUDRATE 9600
 #define FET 2 // Trigger threshold. MOSFET. Actives red LED
 #define GREEN_LED 3 // High when threshold note exceeded
 #define POWER_DUST 4 // pnp bjt powering HPMA
 // scale the threshold with this value. 
-#define SCALE 1.0 // to specify float must have decimal notation
+#define SCALE 1.0 // to specify float, must have decimal notation
 
 // GLOBALS
   time_t t;
-  
+
   //Controlled by Potentiometer
   const int analogInPin = A0;  // Potentiometer Analog ping
   const int analogOutPin = 9; 
   short int sensorValue = 0;        // value read from the pot
   short int outputValue = 0;        // value output to the PWM
   float threshold;
+  float average; 
   
   // Controlled by Dust Sensor
   double sum = 0; // note: maxes at 65,535.
@@ -35,8 +37,11 @@
   unsigned int i; // index for circle
 
 
-//Create an instance of the HPMA115S0 library
+//Create an instance of the HPMA115S0 class
 HPMA115S0 honeywell(Serial1);
+
+//Create an instance of MegunoLink TimePlot class
+TimePlot dust_plot;
 
 void setup() {
   
@@ -61,6 +66,10 @@ void setup() {
     circle[i] = 0;
   }
   i = 0; // i is global, reset to 0
+
+  dust_plot.SetTitle("Dust vs Threshold Over Time");
+  dust_plot.SetXlabel("Time");
+  dust_plot.SetYlabel("Dust Level (%)");
   
   Serial1.begin(BAUDRATE); //begin honeywell comms
   do {
@@ -99,7 +108,21 @@ void loop() {
       time_t lap_time = now() - t;
       Serial.print("lap_time = ");
       Serial.println(lap_time);
-      t = now();
+      t = now(); // time in sec since program started
+
+      // Send data to meguno for plotting
+      // https://www.megunolink.com/documentation/plotting/time-plot-reference/
+      dust_plot.SendData("Dust Level", average);
+      dust_plot.SendData("Threshold Level", SCALE*threshold);
+
+      // Print message to meguno for logging
+      // https://www.megunolink.com/documentation/logging-data/
+      Serial.print("{MESSAGE:|data|");
+      Serial.print(",");
+      Serial.print(SCALE*threshold);
+      Serial.print(",");
+      Serial.print(average);
+      Serial.println("}");
     }
     else {
       i++;
@@ -108,7 +131,7 @@ void loop() {
     //read analog
     //threshold dust level out of 100
     threshold = read_adc() * ( (float)100 / (float)1023 );
-    Serial.println("Threshold = " + String(threshold));
+    Serial.println("Threshold = " + String(threshold) + "%");
 
     // Sum all recent measurements
     int k = 0;
@@ -119,9 +142,8 @@ void loop() {
     }
 
     // Average all recent measurements
-    float average; 
     average = ( (float)sum ) / ( (float)circle_size ); 
-    Serial.println("Sum = " + String(sum));
+    //Serial.println("Sum = " + String(sum));
     Serial.println("Avg = " + String(average));     
     
     
@@ -148,10 +170,6 @@ int read_adc(){
   outputValue = map(sensorValue, 0, 1023, 0, 255);
   // change the analog out value:
   analogWrite(analogOutPin, outputValue);
-
-  // print the results to the Serial Monitor:
-  Serial.print("Potentiometer output = ");
-  Serial.println(outputValue);
 
   // wait 2 milliseconds before the next loop for the analog-to-digital
   // converter to settle after the last reading:
