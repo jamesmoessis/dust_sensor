0321b3261b804dc313c3f5426c80cbd3a3385ce6
Maximus Moessis <maximus.moessis@students.mq.edu.au>
2023-10-08 15:16:37 +1100

    frontend/ contains a bare bones react app project.

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Dust Sensor Controls</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
