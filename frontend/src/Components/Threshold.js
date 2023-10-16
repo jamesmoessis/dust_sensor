ca707b6876e453e7e77e071ba522dd9af307f9d8
Maximus Moessis <maximus.moessis@students.mq.edu.au>
2023-10-16 22:03:41 +1100

    Frontend contains bare minimum functionality (will give it more at a later date) First instance of the API being accessed, the current threshold is displayed after sending a get req to the localhost API

--- frontend/src/Components/Threshold.js
+++ frontend/src/Components/Threshold.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
+import Button from '@mui/material/Button';
+
+import './Slider.css';
 
 const Threshold = () => {
+  
+  const [thresValue, setThres] = useState(null);
+
+  useEffect(() => {     // gets the threshold value to show in the current threshold area. 
+    axios
+    .get("http://localhost:8080/api/settings")
+    .then((res) => {
+      setThres(res.data.threshold);
+    })
+    .catch((error) => {
+      console.log("failed to collect threshold from API", error);
+    })
+  }, [])
+
   return (
     <div className='control' id='threshold-slider'>
-
-        <Box sx={{ width: 300 }}>
-            <h2>Threshold</h2>
-            <Slider 
+      <h2>THRESHOLD</h2>
+        <Box sx={{ width: 300 }} id="slider-container"> 
+          <h3>Current Threshold: {thresValue}</h3>
+            <Slider id="slider"
             defaultValue={0} 
             aria-label="Default" 
             valueLabelDisplay="auto" 
             min={0} 
-            max={1} 
-            step={0.1}
+            max={100} 
+            step={1}
             color='secondary'
             />
         </Box>
+        <Button variant="contained" color="success" id="set-btn">
+          Set
+        </Button>
     </div>
   );
 }
