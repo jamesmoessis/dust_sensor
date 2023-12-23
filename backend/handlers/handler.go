a9c46324fb04479a07be7a435c0414922a972acc
James Moessis <james.moessis1@gmail.com>
2023-12-23 14:26:53 +1100

    instructions on basic auth

--- backend/handlers/handler.go
+++ backend/handlers/handler.go
@@ -33,6 +33,19 @@ func (h *Handler) RouterHandler(ctx context.Context, req *Request) (*Response, e
 }
 
 func (h *Handler) updateSettingsHandler(ctx context.Context, req *Request) (*Response, error) {
+
+	// Pick a username / password
+	// take echo -n "mypassword" | shasum -a 256
+	// make the code take the password's hash (use the golang sha256 lib)
+	// There should be a header incoming that looks like:
+	// Authorization: Basic mypassword
+	// Then take the sha 256 password hash and compare it to what is in the database.
+
+	// If it doesn't have the correct password, then it needs to return
+	// a response &Response {Status: 401, Headers: {"WWW-Authenticate": "Basic realm=\"basic\""}}
+
+	// Store the hash in a variable at first (and we will put it in the database later, maybe change it)
+
 	settings := &Settings{}
 	err := json.Unmarshal([]byte(req.Body), settings)
 	if err != nil {
--- backend/handlers/types.go
+++ backend/handlers/types.go
@@ -3,14 +3,16 @@ package handlers
 import "context"
 
 type Request struct {
-	Body   string
-	Method string
-	Path   string
+	Body    string
+	Method  string
+	Path    string
+	Headers map[string]string
 }
 
 type Response struct {
-	Body   string
-	Status int
+	Body    string
+	Status  int
+	Headers map[string]string
 }
 
 type Settings struct {
